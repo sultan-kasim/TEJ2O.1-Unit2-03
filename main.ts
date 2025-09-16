42deaaa0fa6fb1fee2eabac0a572c322a4d77250
sultan-kasim <sultan.kasim@stu.ocsb.ca>
2025-09-16 13:30:44 -0400

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,14 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2025 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
+ * Created by: Sultan
+ * Created on: Sep 2025
  * This program ...
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.pause(1000)
+//Area and Perimeter 
+
+basic.showString('The rectangle has the dimensions of 5cm & 3cm')
+basic.showString('A=L x W, 3 x 5 = ' + ( 5 * 3).toString(cm))
+basic.showString('The perimeter would be '+ 2(5 + 3()).toString)())
