d1786276bb21ffad488a6f4f8d6293a54c867f60
sultan-kasim <sultan.kasim@stu.ocsb.ca>
2025-09-18 13:07:35 -0400

    Now sayd area and perimeter

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@
  *
  * Created by: Sultan
  * Created on: Sep 2025
- * This program calculates Perimeter and Area
+ * This program calculates Perimeter & Area
 */
 basic.clearScreen()
 basic.pause(1000)
