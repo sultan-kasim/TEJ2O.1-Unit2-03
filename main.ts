12f8bdbe787c3ab34b573340ab8ff0414677debc
sultan-kasim <sultan.kasim@stu.ocsb.ca>
2025-09-18 13:03:44 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,16 +4,17 @@
  * Created on: Sep 2025
  * This program calculates Perimeter and Area
 */
-
-//Area and perimiter of a rectangle
 basic.clearScreen()
 basic.pause(1000)
 
+//Area and perimiter of a rectangle
+
 basic.showString('A rectangle has dimensions 5 cm & 3 cm.')
-basic.showIcon
+basic.showIcon(IconNames.Happy)
+basic.pause(100)
 basic.clearScreen()
 basic.showString('The perimeter would be:' + (2 * (5 + 3)).toString() + 'cm')
-basic.showIcon
+basic.showIcon(IconNames.Happy)
 basic.pause(100)
 basic.clearScreen()
 basic.showString('The Area would be:' + (5 * 3).toString() + 'cm')
